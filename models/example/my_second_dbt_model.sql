13b0afd94c8092deb8e2f15d03bbbc6cc3917f20
Ashok S <linkedinprofile38@gmail.com>
2024-05-21 08:51:30 +0000

    3

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,16 @@
 
 -- Use the `ref` function to select from other models
 
+{{
+    config(
+        materialized = 'incremental'
+    )
+}}
+
+{% if is_incremental() %} 
+  and rank > (select max(rank) from {{this}})
+{% endif %}
+
 select *
 from {{ ref('my_first_dbt_model') }}
-where rank = '1'
+
